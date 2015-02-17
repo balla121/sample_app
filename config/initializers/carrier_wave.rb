34d40d18d7f308334912f7b33efd457d909e1d9d
balla121 <Mkman0123!@#>
2015-02-17 12:59:35 -0500

    fixes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,11 @@
 if Rails.env.production?
-	CarrierWave.configure do |config|
-		config.fog_credentials = {
-
-		#configuration for amazon s3
-		:provider => 'AWS',
-		:aws_access_key_id => ENV['AKIAJWKDZWZYIDKD5LYQ'],
-		:aws_secret_access_key => ENV['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']
-		}
-		config.fog_directory = ENV['weijiesampleapp']
-	end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
